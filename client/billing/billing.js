cea832c5df486f6ffe36d10d44b01619f8271a7c
systemzax <gbabint@msn.com>
2014-12-06 15:11:33 -0500

    Added merchants folder

---
+++ client/billing/billing.js
@@ -0,0 +1,20 @@
+Template.billing.helpers({
+	merchant: function(){
+		return "Some dude";
+	},
+	merchantLogo: function(){
+		return "Some dude's logo";
+	},
+	rate: function(){
+		return "Some rate";
+	},
+	qrCode: function(){
+		return "Some qrCode";
+	},
+	address: function(){
+		return "Some address";
+	},
+	status: function(){
+		return "Some status";
+	}
+});
