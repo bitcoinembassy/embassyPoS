1086889982e01d5c80a5ac6b7967839e6bbaa836
Francis Brunelle <frabrunelle@gmail.com>
2014-12-07 01:25:52 -0500

    display user settings in form

--- client/settings/settings.js
+++ client/settings/settings.js
@@ -1,22 +1,16 @@
-if (Meteor.isClient) {
+Template.settings.events({
+  'click #saveChanges': function(event, template){
+		var merchantName = document.getElementById('merchantName').value;
+		var merchantLogo = document.getElementById('merchantLogo').value;
+	  var commission = document.getElementById('commission').value;
+	  var units = document.getElementById('units').value;
 
-	Template.settings.events({
-    	'click #saveChanges': function(event, template){
+	  //console.log(Meteor.userId());
 
-	    	var merchantName = document.getElementById('merchantName').value;
-			var merchantLogo = document.getElementById('merchantLogo').value;
-	    	var commission = document.getElementById('commission').value;
-	    	var units = document.getElementById('units').value;
+		Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.merchantName': merchantName, 'profile.merchantLogo': merchantLogo, 'profile.commission': commission, 'profile.units': units}}, { validationContext: "updateForm" }, function(error, result) {
 
-	    	//console.log(Meteor.userId());
+		});
 
-			Meteor.users.update({_id: Meteor.userId()}, {$set: {'UserProfile.merchantName': merchantName, 'UserProfile.merchantLogo': merchantLogo, 'UserProfile.commission': commission, 'UserProfile.units': units}}, { validationContext: "updateForm" }, function(error, result) {
+    }
 
-			});
-
-    	}
-
-	});
-
-
-}
+});
