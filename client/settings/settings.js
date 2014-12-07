e6174ecf662cff5c7e0a67bf6d2592c5c700b1a8
systemzax <gbabint@msn.com>
2014-12-07 02:09:44 -0500

    Fixed the loading of units

--- client/settings/settings.js
+++ client/settings/settings.js
@@ -1,14 +1,41 @@
+Status = new Meteor.Collection(null);
+
+Template.settings.helpers({
+	unitTypes: function (){
+
+		var unitTypes = [
+			{value:"bits", caption:"Bits"}, 
+			{value:"btc", caption: "BTC"}
+		];
+
+	    return unitTypes;
+
+    },
+    isSelected: function (){
+
+    	var user = Meteor.users.findOne({_id: Meteor.userId()});
+
+    	if (user != null && user.profile != null && user.profile.units){
+			if (user.profile.units == this.value)
+				return "selected";
+			else
+				return "";
+    	}
+
+    }
+});
+
 Template.settings.events({
-  'click #saveChanges': function(event, template){
+	'click #saveChanges': function(event, template){
 		var merchantName = document.getElementById('merchantName').value;
 		var merchantLogo = document.getElementById('merchantLogo').value;
-	  var commission = document.getElementById('commission').value;
-	  var units = document.getElementById('units').value;
+		var commission = document.getElementById('commission').value;
+		var units = document.getElementById('units').value;
 
-	  //console.log(Meteor.userId());
+		//console.log(Meteor.userId());
 
 		Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.merchantName': merchantName, 'profile.merchantLogo': merchantLogo, 'profile.commission': commission, 'profile.units': units}}, { validationContext: "updateForm" }, function(error, result) {
-
+			Router.go('billing');
 		});
 
     }
